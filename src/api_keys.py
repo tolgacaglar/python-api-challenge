0b8f8a9070e05eef261b81f3ed8a84a480cb8708
Tolga Caglar <tolcaglar@gmail.com>
2020-12-29 23:24:02 -0800

    Testing gitignore after working api keys

--- src/api_keys.py
+++ src/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "cf663b67fa6fe6031298397c7e781087"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCl03XN9F89RIkt3dO8uMeRiCF1wjw8Clk"
